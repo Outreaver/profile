9c5615befe65b8d9b8dba0b24004f5a3da99b610
Bartosz Hejduk <bartoszhejduk2@gmail.com>
2025-04-24 11:05:13 +0200

    Changed profile to hard link

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,5 @@
-C:/Users/Bartosz/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/quick-term.omp.json' | Invoke-Expression
+
+function Make-Link ($target, $link) {
+    New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
