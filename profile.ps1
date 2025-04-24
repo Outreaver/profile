881538e10b8cb031d49af5cb2cb4e2a3c2d6db0c
Bartosz Hejduk <bartoszhejduk2@gmail.com>
2025-04-24 12:20:49 +0200

    Added openssl aliad

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,3 @@
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/quick-term.omp.json' | Invoke-Expression
+
+Set-Alias openssl "C:\Program Files\Git\usr\bin\openssl.exe"
