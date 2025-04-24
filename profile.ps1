9a2f3874187540a4090e76ecd15fd27c2b56e69b
Bartosz Hejduk <bartoszhejduk2@gmail.com>
2025-04-24 11:19:54 +0200

    Changed profile to symbolic link again, but swapped files around

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,1 @@
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/quick-term.omp.json' | Invoke-Expression
-
-function Make-Link ($target, $link) {
-    New-Item -Path $link -ItemType SymbolicLink -Value $target
-}
