4276ca125a9153c870c7aca11017871718d7236a
Bartosz Hejduk <bartoszhejduk2@gmail.com>
2025-08-26 10:48:07 +0200

    Update DBash command to run on empty container

--- DockerCommands.ps1
+++ DockerCommands.ps1
@@ -2,9 +2,24 @@ function DRemoveNoneImages {
     docker images --filter "dangling=true" -q | ForEach-Object { docker rmi $_ }
 }
 
+function DBash {
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string]$Container
+    )
+
+    if ([string]::IsNullOrWhiteSpace($Container)) {
+        docker run -it --rm busybox
+    }
+    else {
+        docker exec -it $Container /bin/sh
+    }
+}
+
 function DockerCommands {
     $commands = @(
-        [Command]::new('DRemoveNoneImages', 'Removes dangling (<none>) images.')
+        [Command]::new('DRemoveNoneImages', 'Removes dangling (<none>) images.'),
+        [Command]::new('DBash', 'Runs bash inside container', @( [CommandParameter]::new('Container', $false)))
     )
 
     return $commands;
